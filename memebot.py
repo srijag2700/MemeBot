b61587cb45428b43b2c189717b9b4baac40cdb59
Srija Gottiparthi <srija.gottiparthi@gmail.com>
2020-03-05 15:50:24 -0500

    Add subreddit name to each meme, mark 2

--- memebot.py
+++ memebot.py
@@ -33,7 +33,7 @@ async def on_ready():
 async def meme(ctx):
     sub = reddit.subreddit(random.choice(memes))
     submission = sub.random()
-    await ctx.send(submission.url + " from " + sub)
+    await ctx.send(submission.url + " from " + sub.name)
 
 """@bot.command()
 async def memes(ctx, num: int):
